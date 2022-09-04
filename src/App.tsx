ba4a9f74b99900616ef6b37bb21eebf0bb8dc0eb
Emoxx09 <khyle.emox@gmail.com>
2022-09-04 19:24:07 +0800

    Added loader and removed app.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import styled from 'styled-components';
-
-import './App.css';
 
 import Navbar from './components/Navbar/index';
 import About from './components/About/index';
@@ -11,18 +9,48 @@ import Work from './components/Work/index';
 import Projects from './components/Projects/index';
 import Contact from './components/Contact/index';
 
+import loaderImage from './Images/loader.gif';
+
+import { Image } from '@chakra-ui/react';
+
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 4500);
+  }, []);
+
   return (
-    <div>
-      <StyledNavbar />
-      <About />
-      <Skills />
-      <Work />
-      <Projects />
-      <Contact />
+    <div className="App">
+      {loading ? (
+        <Loader>
+          {' '}
+          <Image src="/loader.gif" />
+        </Loader>
+      ) : (
+        <div>
+          <StyledNavbar />
+          <About />
+          <Skills />
+          <Work />
+          <Projects />
+          <Contact />
+        </div>
+      )}
     </div>
   );
 }
+
+const Loader = styled.div`
+  background-color: black;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const StyledNavbar = styled(Navbar)`
   position: fixed;
